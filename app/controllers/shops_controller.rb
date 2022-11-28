6c05d317bdcc20935afbd02fe3c8a6506c2b62ff
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-11-28 17:50:07 +0900

    うまく行ってないVer

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,11 +4,12 @@ class ShopsController < ApplicationController
       @client = ::GooglePlaces::Client.new(ENV['GOOGLE_MAP_API'])
       @places = @client.spots_by_query("ラーメン", language: 'ja', types: 'food')
       gon.places = @places
+      # byebug
     else
     @client = ::GooglePlaces::Client.new(ENV['GOOGLE_MAP_API'])
     @places = @client.spots_by_query("#{params[:search]},ラーメン", language: 'ja', types: 'food')
     gon.places = @places
-    # byebug
+    #  byebug
     end
   end
 
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,18 +1,11 @@
 class ToppagesController < ApplicationController
   def index
+    @shop = params[:content]
     # byebug
-    if params[:search].nil?
-      @client = ::GooglePlaces::Client.new(ENV['GOOGLE_MAP_API'])
-      @places = @client.spots_by_query("ラーメン", language: 'ja', types: 'food')
-      gon.places = @places
-    else
-    @client = ::GooglePlaces::Client.new(ENV['GOOGLE_MAP_API'])
-    @places = @client.spots_by_query("#{params[:search]},ラーメン", language: 'ja', types: 'food')
-    gon.places = @places
-    end
   end
 
   def show 
     
   end
+
 end
